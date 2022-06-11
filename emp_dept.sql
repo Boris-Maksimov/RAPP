e58b1020dfa566f0fa6b8796f8d5153c951d9123
badinoff <32521765+badinoff@users.noreply.github.com>
2022-06-11 04:08:50 +0000

    Update emp_dept.sql

--- emp_dept.sql
+++ emp_dept.sql
@@ -1,1 +1,6 @@
-/* List the department of each employee with the following information: employee number, last name, first name, and department name. */
+select distinct emp.emp_no, emp.last_name, emp.first_name, dpt.dept_name 
+	from employees as emp
+join dept_emp as de
+on emp.emp_no = de.emp_no
+join departments dpt
+on de.dep_no = dpt.dept_no
