8442115bcbe9580e9b2e7dba1d7cfb122c9ab258
badinoff <32521765+badinoff@users.noreply.github.com>
2022-06-11 01:12:12 +0000

    Update employee_details.sql

--- employee_details.sql
+++ employee_details.sql
@@ -1,1 +1,3 @@
-/* List the following details of each employee: employee number, last name, first name, gender, and salary. */
+select emp.emp_no, emp.last_name, emp.first_name, emp.gender, sal.salary from employees as emp
+join salaries as sal
+on emp.emp_no = sal.emp_no
