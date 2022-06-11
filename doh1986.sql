c7efc06346d505ba1429c0b8b57c4c7f59295a62
badinoff <32521765+badinoff@users.noreply.github.com>
2022-06-11 01:22:03 +0000

    Update doh1986.sql

--- doh1986.sql
+++ doh1986.sql
@@ -1,1 +1,3 @@
-/* List employees who were hired in 1986. */
+select emp.hire_date, emp.first_name, emp.last_name from employees as emp
+where emp.hire_date between '1986-01-01' and '1986-12-31'
+order by emp.hire_date asc
